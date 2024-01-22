e79a1905f4f21c702ac2f0729bf3b0be46fc8460
vlad3 <vlad3344.sad@gmail.com>
2024-01-22 16:44:45 +0300

    =)

--- app/src/main/java/com/example/expertsystem/StartActivity.kt
+++ app/src/main/java/com/example/expertsystem/StartActivity.kt
@@ -1,43 +1,42 @@
 package com.example.expertsystem
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.expertsystem.currency.CurrencyFragment
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.exceptions.ClientException
 
 class StartActivity : Fragment() {
+
+    private lateinit var autoCompleteTextView: AutoCompleteTextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.start_fragment, container, false)
-    }
+        val view = inflater.inflate(R.layout.start_fragment, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val editText = view.findViewById<EditText>(R.id.editText)
+        autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView)
         val button = view.findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            val enteredText = editText.text.toString()
+            val enteredText = autoCompleteTextView.text.toString()
 
-            val keywords = arrayOf(
-                "Диагноз", "Посчитай", "Психология", "Вычисли", "Болезнь","class","Кто это","Валюта","Калькулятор процента по вкладу"
-            )
+            val keywords = getKeyWords().toTypedArray()
 
             val foundKeyword = keywords.find { enteredText.contains(it) }
 
             if (foundKeyword != null) {
                 when (foundKeyword) {
-                    "Посчитай"-> {
+                    "Посчитай" -> {
                         val mathExpression = enteredText.replace("Посчитай", "").trim()
                         val mathFragment = MathFragment()
 
@@ -52,11 +51,6 @@ class StartActivity : Fragment() {
                     }
                     "Кто это" -> {
                         val whoIsFragment = WhoIsFragment()
-
-                        val args = Bundle()
-                        args.putString("query", enteredText.replace("кто это", "").trim())
-                        whoIsFragment.arguments = args
-
                         parentFragmentManager.beginTransaction()
                             .replace(R.id.fragmentContainer, whoIsFragment)
                             .addToBackStack(null)
@@ -88,6 +82,44 @@ class StartActivity : Fragment() {
                 Toast.makeText(requireContext(), "Тема не найдена", Toast.LENGTH_LONG).show()
             }
         }
+
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up AutoCompleteTextView with suggestions
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, getKeyWords())
+        autoCompleteTextView.setAdapter(adapter)
+    }
+
+    fun getKeyWords(): List<String> {
+        val questions = mutableListOf<String>()
+        val thread = Thread {
+            try {
+                val driver = GraphDatabase.driver(
+                    "neo4j+ssc://ebce1fa4.databases.neo4j.io:7687",
+                    AuthTokens.basic("neo4j", "0hUpEQy1HUGSvMgGtKtlb3rojnWf5axLk-UkzBhDBaY")
+                )
+                driver.use { driver ->
+                    val session = driver.session()
+                    val result =
+                        session.run("MATCH (k:Keywords) RETURN k.text AS keywords")
+
+                    while (result.hasNext()) {
+                        val record = result.next()
+                        val questionText = record["keywords"].asString()
+                        questions.add(questionText)
+                    }
+                    session.close()
+                }
+            } catch (e: ClientException) {
+                e.printStackTrace()
+            }
+        }
+        thread.start()
+        thread.join()
+        return questions
+    }
 }
--- app/src/main/java/com/example/expertsystem/WhoIsFragment.kt
+++ app/src/main/java/com/example/expertsystem/WhoIsFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.GraphDatabase
-import org.neo4j.driver.types.Node
 import org.neo4j.driver.exceptions.ClientException
 
 class WhoIsFragment : Fragment() {
+
+    private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var nameTextView: TextView
+    private lateinit var biographyTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,23 @@ class WhoIsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        autoCompleteTextView = view.findViewById(R.id.autoCompletePerson)
+        nameTextView = view.findViewById(R.id.nameTextView)
+        biographyTextView = view.findViewById(R.id.biographyTextView)
+        val button = view.findViewById<Button>(R.id.button)
+
+        val persons = getPersonSuggestions()
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, persons)
+        autoCompleteTextView.setAdapter(adapter)
+
+        button.setOnClickListener {
+            val enteredPerson = autoCompleteTextView.text.toString()
+            queryPersonDetails(enteredPerson)
+        }
+    }
+
+    private fun queryPersonDetails(person: String) {
         val thread = Thread {
             try {
                 val driver = GraphDatabase.driver(
@@ -31,28 +54,40 @@ class WhoIsFragment : Fragment() {
                 driver.use { driver ->
                     val session = driver.session()
                     val result = session.run("""
-                    MATCH (person:Person)
-                    RETURN person.fullName AS fullName, person.biography AS biography
+                    MATCH (person:Person {fullName: '$person'})
+                    RETURN person AS person
                 """)
-                    val nameTextView = view.findViewById<TextView>(R.id.nameTextView)
-                    val biographyTextView = view.findViewById<TextView>(R.id.biographyTextView)
 
                     if (result.hasNext()) {
                         val record = result.next()
                         if (record.containsKey("person")) {
-                            val personNode = record["person"] as Node
-                            val personName = personNode["fullName"].asString()
-                            val personBiography = personNode["biography"].asString()
+                            val personValue = record["person"]
+                            if (personValue.type().name() == "NODE") {
+                                val personNode = personValue.asNode()
+                                val personName = personNode["fullName"].asString()
+                                val personBiography = personNode["biography"].asString()
 
-                            nameTextView.text = "ФИО: $personName"
-                            biographyTextView.text = "Биография: $personBiography"
+                                activity?.runOnUiThread {
+                                    nameTextView.text = "ФИО: $personName"
+                                    biographyTextView.text = "Биография: $personBiography"
+                                }
+                            } else {
+                                activity?.runOnUiThread {
+                                    nameTextView.text = "Такого человека не существует"
+                                    biographyTextView.text = ""
+                                }
+                            }
                         } else {
+                            activity?.runOnUiThread {
+                                nameTextView.text = "Такого человека не существует"
+                                biographyTextView.text = ""
+                            }
+                        }
+                    } else {
+                        activity?.runOnUiThread {
                             nameTextView.text = "Такого человека не существует"
                             biographyTextView.text = ""
                         }
-                    } else {
-                        nameTextView.text = "Такого человека не существует"
-                        biographyTextView.text = ""
                     }
                     session.close()
                 }
@@ -62,6 +97,35 @@ class WhoIsFragment : Fragment() {
         }
         thread.start()
         thread.join()
+    }
+
+
+    private fun getPersonSuggestions(): List<String> {
+        val persons = mutableListOf<String>()
+        val thread = Thread {
+            try {
+                val driver = GraphDatabase.driver(
+                    "neo4j+ssc://ebce1fa4.databases.neo4j.io:7687",
+                    AuthTokens.basic("neo4j", "0hUpEQy1HUGSvMgGtKtlb3rojnWf5axLk-UkzBhDBaY")
+                )
+                driver.use { driver ->
+                    val session = driver.session()
+                    val result = session.run("MATCH (person:Person) RETURN person.fullName AS fullName")
+
+                    while (result.hasNext()) {
+                        val record = result.next()
+                        val personName = record["fullName"].asString()
+                        persons.add(personName)
+                    }
+                    session.close()
+                }
+            } catch (e: ClientException) {
+                e.printStackTrace()
+            }
+        }
+        thread.start()
+        thread.join()
+        return persons
     }
 
 }
